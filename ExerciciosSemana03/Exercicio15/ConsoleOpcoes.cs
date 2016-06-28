bd9f46e59f624ee9fc275ce0617e077b8c0f7e63
sockadm <sock.adm@gmail.com>
2016-06-27 21:57:49 -0300

    Exercicio 15
    Finalizado

---
+++ ExerciciosSemana03/Exercicio15/ConsoleOpcoes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio15_console
+
+{
+    public class ConsoleOpcoes
+    {
+        internal int console_listar()
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("|(1) - Inserir item           |");
+            Console.WriteLine("|(2) - Buscar itens           |");
+            Console.WriteLine("|(3) - Total item             |");
+            Console.WriteLine("|(4) - Excluir item           |");
+            Console.WriteLine("|(0) - Sair do programa       |");
+            Console.WriteLine("-------------------------------");
+            Console.Write("Informe a opção desejada: ");
+            int saida = Convert.ToInt32(Console.ReadLine());
+            return saida;
+        }
+
+        internal void aperte_para_continuar()
+        {
+            Console.WriteLine("Aperte uma tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosSemana03/Exercicio15/Exercicio15.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Exercicio15_console;
+
+namespace Exercicio15
+{
+    class Exercicio15
+    {
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+
+            ConsoleOpcoes ex15 = new ConsoleOpcoes();
+            int quit = 1;
+
+            while (quit != 0)
+            {
+
+
+                int opcao = ex15.console_listar();
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Write("Qual o nome do item a ser inserido: ");
+                        lista.Add(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.Write("Informe o item procurado: ");
+                        string procurado = Console.ReadLine();
+                        if (lista.Find(x => lista.Contains(procurado)) == null) Console.WriteLine("Item não encontrado");
+                        else Console.WriteLine(lista.Find(x => lista.Contains(procurado)));
+                        ex15.aperte_para_continuar();            
+                        break;
+                    case 3:
+                        Console.WriteLine("Total de items = " + lista.Count);
+                        ex15.aperte_para_continuar();
+                        break;
+                    case 4:
+                        lista.Sort(); //ordena para facilitar a exclusão
+                        foreach (var item in lista)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        Console.WriteLine("Qual item deseja excluir?");
+                        lista.Remove(Console.ReadLine());
+                        ex15.aperte_para_continuar();
+                        break;
+                    case 0:
+                        //Console.WriteLine("Sair do programa");
+                        quit = 0;
+                        break;
+                    default:
+
+                        break;
+                }
+
+            }
+
+            Console.Clear();
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Lista Final de itens:");
+            lista.Sort();
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            ex15.aperte_para_continuar();
+
+        }
+
+
+    }
+
+    
+}
+
